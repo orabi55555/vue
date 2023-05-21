5f13dfb17fd30d2b473582c7b909aede2dc7ca0c
Ahmed Orabi <105594989+orabi55555@users.noreply.github.com>
2023-05-21 13:31:06 +0300

    Add files via upload

---
+++ lab1/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <FormComp/>
+
+    
+</template>
+
+<script>
+
+import FormComp from './components/FormComp.vue';
+export default {
+  name: 'App',
+  components: {
+  FormComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
